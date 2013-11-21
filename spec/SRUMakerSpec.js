9ebb29ae34e210b3ee4adc509f6007297ccff6b9
zipper <bjorn@aidium.com>
2013-11-21 01:18:06 +0100

    Initial commit

---
+++ spec/SRUMakerSpec.js
@@ -0,0 +1,194 @@
+describe("Hello test", function() {
+    it("says hello", function() {
+        expect(helloTest()).toEqual("Hello test!");
+    });
+});
+
+describe("Databeskrivning", function() {
+        var databeskrivning = new Databeskrivning();
+        
+        it("Ska ha en start", function() {
+                expect(databeskrivning.databeskrivningStart()).toEqual("#DATABESKRIVNING_START");
+        });
+
+        it("Ska ha ett slut", function() {
+                expect(databeskrivning.databeskrivningSlut()).toEqual("#DATABESKRIVNING_SLUT");
+        });
+
+        it("Ska omsluta innehållet", function() {
+                var innehåll = databeskrivning.generera();
+                expect(innehåll[0]).toEqual("#DATABESKRIVNING_START");
+                expect(innehåll[innehåll.length - 1]).toEqual("#DATABESKRIVNING_SLUT");
+        });
+
+        describe("Generering av obligatoriska uppgifter", function() {
+                it("Ska ha dagens datum och tid i rätt format", function() {
+                        expect(databeskrivning.skapad()).toMatch(/#SKAPAD \d\d\d\d\d\d\d\d \d\d\d\d\d\d/);
+                });
+
+                it("Ska ha produkt som SRU2013", function() {
+                        expect(databeskrivning.produkt()).toEqual("#PRODUKT SRU2013");
+                });
+
+                it("Ska ha perioden", function() {
+                        expect(databeskrivning.period()).toEqual("#PERIOD 2013");
+                });
+
+                it("Ska ha program som SRU-Maker", function() {
+                        expect(databeskrivning.program()).toEqual("#PROGRAM SRU-Maker");
+                });
+
+                it("Ska ha filnamn som BLANKETTER.SRU", function() {
+                        expect(databeskrivning.filnamn()).toEqual("#FILNAMN BLANKETTER.SRU");
+                });
+        });
+});
+
+describe("Medieleverantören", function() {
+        var medielev = new Medielev("198105198256", "Björn Palmqvist", "90629", "Umeå");
+        
+        it("Ska ha en start", function() {
+                expect(medielev.medielevStart()).toEqual("#MEDIELEV_START");
+        });
+
+        it("Ska ha ett slut", function() {
+                expect(medielev.medielevSlut()).toEqual("#MEDIELEV_SLUT");
+        });
+
+        it("Ska omsluta innehållet", function() {
+                var innehåll = medielev.generera();
+                expect(innehåll[0]).toEqual("#MEDIELEV_START");
+                expect(innehåll[innehåll.length - 1]).toEqual("#MEDIELEV_SLUT");
+        });
+
+        describe("Generering av obligatoriska uppgifter", function() {
+                it("Ska ha orgnr", function() {
+                        expect(medielev.orgNr()).toMatch(/#ORGNR \d\d\d\d\d\d\d\d\d\d/);
+                });
+
+                it("Ska ha namn", function() {
+                        expect(medielev.namn()).toMatch(/#NAMN [a-zA-ZåÅäÄöÖ ]+/);
+                });
+
+                it("Ska ha postnummer", function() {
+                        expect(medielev.postNr()).toMatch(/#POSTNR \d\d\d\d\d/);
+                });
+
+                it("Ska ha postorten", function() {
+                        expect(medielev.postOrt()).toMatch(/#POSTORT [a-zA-ZåÅäÄöÖ]+/);
+                });
+        });
+});
+
+
+describe("Info SRU ska gå att generera", function() {
+        it("Generera INFO_SRU", function() {
+                var infoSru = genereraInfoSru("198105198256", "Björn Palmqvist", "90629", "Umeå");
+                expect(infoSru).toMatch(/#ORGNR 198105198256/);
+                expect(infoSru).toMatch(/#NAMN Björn Palmqvist/);
+                expect(infoSru).toMatch(/#POSTORT Umeå/);
+                expect(infoSru).toMatch(/#FILNAMN BLANKETTER.SRU/);
+        });
+});
+
+describe("Hantering av Blankett", function() {
+        var blankett;
+        var blankettId = "K10-2013";
+        var idOrgNr = "198105198256";
+
+        beforeEach(function() {
+                blankett = new Blankett(blankettId, idOrgNr);
+        });
+
+        it("Ska ha en början med id", function() {
+                expect(blankett.start()).toEqual("#BLANKETT " + blankettId);
+        });
+
+        it("Ska ha en identitet med rätt format", function() {
+                expect(blankett.identitet()).toMatch(/#IDENTITET \d\d\d\d\d\d\d\d\d\d\d\d \d\d\d\d\d\d\d\d \d\d\d\d\d\d/);
+        });
+
+        it("Ska ha ett slut", function() {
+                expect(blankett.slut()).toEqual("#BLANKETTSLUT");
+        });
+
+        it("De ska gå att generera blanketten", function() {
+                var innehåll = blankett.generera();
+
+                expect(innehåll[0]).toEqual(blankett.start());
+                expect(innehåll[1]).toEqual(blankett.identitet());
+                expect(innehåll[2]).toEqual(blankett.slut());
+        });
+
+        it("Ska gå att lägga till namn", function() {
+                blankett.infogaNamn("Björn Palmqvist");
+                var innehåll = blankett.generera();
+
+                expect(innehåll[2]).toEqual("#NAMN Björn Palmqvist");
+        });
+
+        it("Ska gå att lägga till en uppgift", function() {
+                blankett.nyUppgift("4530", "169780001096");
+                var innehåll = blankett.generera();
+
+                expect(innehåll[2]).toEqual("#UPPGIFT 4530 169780001096");
+        });
+
+        it("Ska gå att lägga till systeminfo", function() {
+                blankett.läggtillSystemInfo("klarmarkerad 20130414 u a");
+                var innehåll = blankett.generera();
+
+                expect(innehåll[2]).toEqual("#SYSTEMINFO klarmarkerad 20130414 u a");
+        });
+});
+
+describe("Filer ska hantera blanketterna", function() {
+        var idOrgNr = "198105198256";
+
+        var blankett1Id = "K10-2013";
+        var blankett1 = new Blankett(blankett1Id, idOrgNr);
+        
+        var blankett2Id = "ANST-2013";
+        var blankett2 = new Blankett(blankett2Id, idOrgNr);
+
+        it("Ska gå att generera en fil med en blankett", function() {
+                var fil = new Fil();
+                fil.infogaBlankett(blankett1);
+
+                var innehåll = fil.generera();
+                expect(innehåll[0]).toEqual("#BLANKETT K10-2013");
+                expect(innehåll[1]).toMatch(/#IDENTITET 198105198256 \d\d\d\d\d\d\d\d \d\d\d\d\d\d/);
+                expect(innehåll[2]).toEqual("#BLANKETTSLUT");
+                expect(innehåll[3]).toEqual("#FIL_SLUT");
+        });
+
+        it("Ska gå att generera en fil med flera blanketter", function() {
+                var fil = new Fil();
+                fil.infogaBlankett(blankett1);
+                fil.infogaBlankett(blankett2);
+
+                var innehåll = fil.generera();
+                expect(innehåll[0]).toEqual("#BLANKETT K10-2013");
+                expect(innehåll[1]).toMatch(/#IDENTITET 198105198256 \d\d\d\d\d\d\d\d \d\d\d\d\d\d/);
+                expect(innehåll[2]).toEqual("#BLANKETTSLUT");
+                expect(innehåll[3]).toEqual("#BLANKETT ANST-2013");
+                expect(innehåll[4]).toMatch(/#IDENTITET 198105198256 \d\d\d\d\d\d\d\d \d\d\d\d\d\d/);
+                expect(innehåll[5]).toEqual("#BLANKETTSLUT");
+                expect(innehåll[6]).toEqual("#FIL_SLUT");
+        });
+
+        describe("BLAKETTER.SRU ska gå att generera", function() {
+        
+                it("Generera BLANKETTER.SRU", function() {
+                        var fil = new Fil();
+                        fil.infogaBlankett(blankett1);
+                
+                        var blanketterSru = genereraBlanketterSru(fil);
+
+                        expect(blanketterSru).toMatch(/#BLANKETT K10-2013/);
+                        expect(blanketterSru).toMatch(/#IDENTITET 198105198256 \d\d\d\d\d\d\d\d \d\d\d\d\d\d/);
+                        expect(blanketterSru).toMatch(/#BLANKETTSLUT/);
+                        expect(blanketterSru).toMatch(/#FIL_SLUT/);
+                });
+        });
+});
---
+++ src/SRUMaker.js
@@ -0,0 +1,193 @@
+function helloTest() {
+    return "Hello test!";
+}
+
+
+function Databeskrivning() {
+
+    this.generera = function() {
+        var innehåll = [];
+        innehåll.push(this.databeskrivningStart());
+        innehåll.push(this.produkt());
+        innehåll.push(this.period());
+        innehåll.push(this.skapad());
+        innehåll.push(this.filnamn());
+        innehåll.push(this.databeskrivningSlut());
+        return innehåll;
+    }
+
+    this.databeskrivningStart = function() {
+        return "#DATABESKRIVNING_START";
+    }
+
+    this.databeskrivningSlut = function() {
+        return "#DATABESKRIVNING_SLUT";
+    }
+
+    this.skapad = function() {
+            var datum = new Date();
+            return "#SKAPAD " + formatDatum(datum) + " " + formatTid(datum);
+
+    }
+
+    this.produkt = function() {
+            return "#PRODUKT SRU2013";
+    }
+
+    this.period = function() {
+            return "#PERIOD 2013";
+    }
+
+    this.program = function() {
+            return "#PROGRAM SRU-Maker";
+    }
+
+    this.filnamn = function() {
+            return "#FILNAMN BLANKETTER.SRU";
+    }
+}
+
+function formatDatum(datum) {
+        return "" + datum.getFullYear() + (datum.getMonth() + 1) + datum.getDate();
+}
+
+function formatTid(datum) {
+        return "" + inledMedNoll(datum.getHours()) + inledMedNoll(datum.getMinutes()) + inledMedNoll(datum.getSeconds());
+}
+
+function inledMedNoll(aTal) {
+        var tal = "" + aTal;
+        if (tal.length == 1) 
+                return tal = "0" + tal;
+        return tal;
+}
+
+function Medielev(aOrgNr, aNamn, aPostNr, aPostOrt) {
+    this.varOrgNr = aOrgNr;
+    this.varNamn = aNamn;
+    this.varPostNr = aPostNr;
+    this.varPostOrt = aPostOrt;
+
+    this.generera = function() {
+        var innehåll = [];
+        innehåll.push(this.medielevStart());
+        innehåll.push(this.orgNr());
+        innehåll.push(this.namn());
+        innehåll.push(this.postNr());
+        innehåll.push(this.postOrt());
+        innehåll.push(this.medielevSlut());
+        return innehåll;
+    }
+
+    this.medielevStart = function() {
+        return "#MEDIELEV_START";
+    }
+
+    this.medielevSlut = function() {
+        return "#MEDIELEV_SLUT";
+    }
+
+    this.orgNr = function() {
+            return "#ORGNR " + this.varOrgNr;
+    }
+
+    this.namn = function() {
+            return "#NAMN " + this.varNamn;
+    }
+
+    this.postNr = function() {
+            return "#POSTNR " + this.varPostNr;
+    }
+
+    this.postOrt = function() {
+            return "#POSTORT " + this.varPostOrt;
+    }
+}
+
+function Blankett(aBlankettId, aIdOrgNr) {
+        this.blakettId = aBlankettId;
+        this.idOrgNr = aIdOrgNr;
+        this.namn = null;
+        this.uppgifter = [];
+
+        this.start = function() {
+                return "#BLANKETT " + this.blakettId;
+        }
+
+        this.identitet = function() {
+                var datum = new Date();
+                return "#IDENTITET " + this.idOrgNr + " " + formatDatum(datum) + " " + formatTid(datum); 
+        }
+
+        this.slut = function() {
+                return "#BLANKETTSLUT";
+        }
+
+        this.generera = function() {
+                var innehåll = [];
+                innehåll.push(this.start());
+                innehåll.push(this.identitet());
+                if (this.namn !== null) {
+                        innehåll.push(this.namn);
+                }
+                this.uppgifter.forEach(function(post) {
+                        innehåll.push(post);
+                });
+                innehåll.push(this.slut());
+                return innehåll;
+        }
+
+        this.infogaNamn = function(aNamn) {
+                this.namn = "#NAMN " + aNamn;
+        }
+
+        this.uppgift = function(id, värde) {
+                return "#UPPGIFT " + id + " " + värde;
+        }
+
+        this.nyUppgift = function(id, värde) {
+                this.uppgifter.push(this.uppgift(id, värde));
+        }
+
+        this.systemInfo = function(värde) {
+                return "#SYSTEMINFO " + värde;
+        }
+
+        this.läggtillSystemInfo = function(värde) {
+                this.uppgifter.push(this.systemInfo(värde));
+        }
+}
+
+function Fil() {
+        this.blanketter = [];
+        this.infogaBlankett = function(blankett) {
+                this.blanketter.push(blankett);
+        }
+
+
+        this.generera = function() {
+                var innehåll = [];
+                this.blanketter.forEach(function(blankett) {
+                        innehåll = innehåll.concat(blankett.generera());
+                });
+                innehåll.push("#FIL_SLUT");
+                return innehåll;
+        }
+}
+
+function genereraInfoSru(aOrgNr, aNamn, aPostNr, aPostOrt) {
+        var databeskrivning = new Databeskrivning();
+        var medialev = new Medielev(aOrgNr, aNamn, aPostNr, aPostOrt);
+
+        var innehåll = databeskrivning.generera();
+        innehåll = innehåll.concat(medialev.generera());
+
+        var infoSru = innehåll.join("<br/>");
+        return infoSru;
+}
+
+function genereraBlanketterSru(fil) {
+       var innehåll = fil.generera();
+       blanketterSru = innehåll.join("<br/>");
+       return blanketterSru;
+}
